b6815f50245a5166c3bafe0ca354e82fea9152b6
arihanv <arihanvaranasi@gmail.com>
2025-03-01 05:00:36 -0800

    update: deepseek call

---
+++ main.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+
+client = OpenAI(
+    base_url="https://api--openai-vllm--dbh9m5jlzc5l.code.run/v1",
+    api_key="EMPTY"
+)
+
+completion = client.chat.completions.create(
+  model="deepseek-ai/DeepSeek-R1-Distill-Qwen-32B",
+  stream=True,
+  messages=[
+    {"role": "user", "content": "What is the capital of France?"}
+  ]
+)
+
+for chunk in completion:
+    print(chunk.choices[0].delta.content, end="", flush=True)
